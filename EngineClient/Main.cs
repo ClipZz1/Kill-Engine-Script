9433dd64b4824d02f8667da5e7466a94bccb40d2
ClipZz1 <70108862+ClipZz1@users.noreply.github.com>
2021-02-17 13:47:57 -0800

    Add files via upload

---
+++ EngineClient/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.UI;
+using CitizenFX.Core.Native;
+using static CitizenFX.Core.Native.API;
+
+namespace EngineClient
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+            EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
+            TriggerEvent("chat:addSuggestion", "/killeng", "Kill's The Vehicle's Engine.");
+            API.RegisterCommand("killeng", new Action(KillEngine), false);
+        }
+
+        private void OnClientResourceStart(string resourceName)
+        {
+            if (GetCurrentResourceName() != resourceName) return;
+
+            Debug.WriteLine($"The resource {resourceName} has been loaded on the Client");
+        }
+
+        private void KillEngine()
+        {
+            API.SetVehicleEngineHealth(GetVehiclePedIsUsing(PlayerPedId()), -4000);
+
+            Screen.ShowNotification("~r~Engine Dying");
+        }
+    }
+}
---
+++ EngineClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EngineClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EngineClient")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bc2ebb8e-9f10-4677-b352-aed3eb2c4d60")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
